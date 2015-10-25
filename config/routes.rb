726a75b33f3d3fbfc9c0f618941a6f8e5f704833
Diyar CAN <diyar.can1@gmail.com>
2015-10-26 00:06:22 +0100

    Including Users and Microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
 
-  get 'welcome/index'
+  resources :microposts
 
-	root 'welcome#index'
+  resources :users
+
+	root 'users#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
