eb52334ef3ccda81cd1605d1b6ac5389685ee98c
Diyar CAN <diyar.can1@gmail.com>
2015-10-25 22:51:46 +0100

    Error 404 Heroku F

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
 	resources :application_controller
 	root 'application_controller#index'
+	get '/' => 'application_controller#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
